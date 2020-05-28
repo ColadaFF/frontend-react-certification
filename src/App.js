7171bf7c126ee9942a3afe8b5158307894e49a31
Cristian Barrientos Montoya <cbarrientos@ias.com.co>
2020-05-28 16:53:31 -0500

    keycloak configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { KeycloakProvider } from "@react-keycloak/web";
+import keycloak, { config as keyCloakProps } from "./keycloak";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <KeycloakProvider keycloak={keycloak} {...keyCloakProps}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </KeycloakProvider>
   );
 }
 
---
+++ src/keycloak.js
@@ -0,0 +1,20 @@
+import Keycloak from "keycloak-js";
+
+// Setup Keycloak instance as needed
+// Pass initialization options as required or leave blank to load from 'keycloak.json'
+const keycloak = new Keycloak({
+  clientId: "frontend",
+  realm: "orders",
+  url: "http://localhost:8090/auth",
+});
+
+export const config = {
+  initConfig: {
+    onLoad: "login-required",
+  },
+  onTokens(tokens) {
+    console.log({ tokens });
+  },
+};
+
+export default keycloak;
